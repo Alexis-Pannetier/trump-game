e2bb551b24fe0f6567ee113c83b47fb9be078fd6
Alexis Pannetier <alexis.pannetier@hotmail.fr>
2020-09-30 16:16:21 +0200

    first commit

---
+++ Assets/Scripts/Tracker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Tracker : MonoBehaviour
+{
+    public GameObject target;
+    /*private float range = 10;*/
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /*float this.transform.position.x;
+        float this.transform.position.y;
+        float targetX = target.transform.position.x;
+        float targetY = target.transform.position.y;
+
+
+        if ((targetX - this.transform.position.x) > range)
+        {
+            x = targetX - range;
+
+         else if ((targetY - this.transform.position.y) > range)
+        {
+            y = targetY - range;
+        }
+
+
+        this.transform.position = new Vector3(x, y, this.transform.position.z);*/
+
+        this.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+        }
+}
